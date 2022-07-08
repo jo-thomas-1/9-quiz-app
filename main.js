5cc312776814024967473c22c49e3815882c4072
Jo Thomas <jothomas1996@gmail.com>
2022-07-08 21:59:44 +0530

    added js function to stop page load

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.clear();
+
+function calculate_score()
+{
+	event.preventDefault(); // to cancel form submit
+
+	console.log("Form Submitted");
+}
+
+// event listener for submit button
+let submit_button = document.getElementById("submit_button");
+submit_button.addEventListener("click", calculate_score);
